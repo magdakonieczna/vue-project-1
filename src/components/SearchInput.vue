5d3d48225c6879938a0f8119b1146c48e7beb592
magdakonieczna <magda.konieczna@ultrano.pl>
2020-03-20 11:54:06 +0100

    Adding background and styling input etc

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="searchWrapper">
+    <div class="search">
+      <input id="search" type="search" v-model="searchValue" @input="handleInput" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+};
+</script>
+
+<style lang="scss" scoped>
+  .searchWrapper {
+    margin-top: 50px;
+    .search {
+      width: 250px;
+      display: flex;
+      flex-direction: column;
+      input {
+        background: none;
+        height: 30px;
+        border: 0;
+        border-bottom: 1px solid black;
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input id="search" type="search" v-model="searchValue" @input="handleInput" />
-    </div>
-    <ul>
-      <li v-for="item in results" :key="item.data[0].nasa_id">
-        <p>{{ item.data[0].description }}</p>
-      </li>
-    </ul>
+    <Claim />
+    <SearchInput />
   </div>
+<!--    <ul>-->
+<!--      <li v-for="item in results" :key="item.data[0].nasa_id">-->
+<!--        <p>{{ item.data[0].description }}</p>-->
+<!--      </li>-->
+<!--    </ul>-->
 </template>
 
 <script>
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
 export default {
   name: 'Home',
+  components: {
+    Claim,
+    SearchInput,
+  },
   data() {
     return {
       searchValue: '',
@@ -41,24 +45,17 @@ export default {
 <style lang="scss" scoped>
   .wrapper {
     width: 100%;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     margin: 0;
     padding: 30px;
-    .search {
-      width: 250px;
-      display: flex;
-      flex-direction: column;
-      label {
-        font-family: Montserrat, sans-serif;
-      }
-      input {
-        height: 30px;
-        border: 0;
-        border-bottom: 1px solid black;
-      }
-    }
+    background-image: url("../assets/moon.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
   }
 
 </style>
